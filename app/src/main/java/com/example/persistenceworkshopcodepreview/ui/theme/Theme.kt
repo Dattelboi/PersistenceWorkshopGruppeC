728f2a8f30bebc3fd70ae1e2bfed061e511a6a49
TimoEngel <timo.engel@appmatics.de>
2024-06-22 12:58:44 +0200

    colors, orange fetzt einfach

--- app/src/main/java/com/example/persistenceworkshopcodepreview/ui/theme/Theme.kt
+++ app/src/main/java/com/example/persistenceworkshopcodepreview/ui/theme/Theme.kt
@@ -13,28 +13,36 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Color(0xFFBB86FC),
-    secondary = Color(0xFF03DAC6)
+    primary = Color(0xFFFF8C00), // Dunkles Orange
+    secondary = Color(0xFFFFA500) // Helles Orange
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Color(0xFF6200EE),
-    secondary = Color(0xFF03DAC6)
+    primary = Color(0xFFFFA500), // Helles Orange
+    secondary = Color(0xFFFF8C00) // Dunkles Orange
 )
 
 @Composable
 fun PersistenceWorkshopCodePreviewTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
 
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
 
